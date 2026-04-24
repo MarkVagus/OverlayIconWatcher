928071761f6482a55ccd9d63fa362718c5c827a3
Julius Haman <j.haman@smf.de>
2026-04-24 12:17:18 +0200

    Umstellung auf Serilog (rollierend pro Tag)

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System.Reflection;
 
 namespace OverlayIconWatcher;
 
 internal class Program
 {
-    internal static string ProgramInfo => $"{Assembly.GetEntryAssembly()?.GetName().Name} {Assembly.GetEntryAssembly()?.GetName().Version}";
+	internal static string ProgramInfo => $"{Assembly.GetEntryAssembly()?.GetName().Name} {Assembly.GetEntryAssembly()?.GetName().Version}";
 
-    static void Main(string[] args)
-    {
-        try
-        {
-            Host.CreateDefaultBuilder(args)
-                .UseWindowsService() // ← sorgt dafür, dass als Windows-Dienst gearbeitet wird
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddLog4Net("log4net.config");
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                })
-                .Build()
-                .Run();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            throw;
-        }
-    }
+	static void Main(string[] args)
+	{
+		try
+		{
+			Host.CreateDefaultBuilder(args)
+				.UseWindowsService() // ← sorgt dafür, dass als Windows-Dienst gearbeitet wird
+				.UseSerilog((context, services, configuration) =>
+				{
+					configuration.ReadFrom.Configuration(context.Configuration);
+				})
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddHostedService<Worker>();
+				})
+				.Build()
+				.Run();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.ToString());
+			throw;
+		}
+	}
 }
